94d47b3676e612d93014e9f04dfba569bbc7f1b5
Sohom Ghosh <sohom.official21@gmail.com>
2025-01-27 05:05:51 +0530

    add the cab -form functionality in the backend.

--- PlanNGo-Hub-Test/src/app/cab/services/admin-cab.service.ts
+++ PlanNGo-Hub-Test/src/app/cab/services/admin-cab.service.ts
@@ -1,9 +1,17 @@
+// admin-cab.service.ts
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { CabCardDetails } from '../model/cabcard-details';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class AdminCabService {
+  private apiUrl = 'http://localhost:3000/CabCardDetailsList';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  createNewCab(cabData: CabCardDetails): Observable<CabCardDetails> {
+    return this.http.post<CabCardDetails>(this.apiUrl, cabData);
+  }
 }
+
